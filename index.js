0fccb39774d8a4637e65fba84d6b645e48d16b63
DESKTOP-2CCSS00\Max <maxweijers@hotmail.nl>
2020-05-14 11:32:11 +0200

    fix?

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ app.use(compression());
 //use public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res, next){
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'orign, x-requested-width, Content-Type, Accept');
-    next();
-});
+// app.use(function(req, res, next){
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'orign, x-requested-width, Content-Type, Accept');
+//     next();
+// });
 
 app.get(['/', '/randomize'], (req, res) => {
     res.render('overview', {characterNames:randomFunctions.getRandomNames(characters)});
